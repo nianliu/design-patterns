2b59b5df8081cfb224a901c1b6389d60f04bd816
Nian Liu <nliu@bol.com>
2020-05-12 09:31:28 +0200

    factory pattern

---
+++ design-patterns-kotlin/src/main/kotlin/com/github/nianliu/creational_patterns/FactoryMethod.kt
@@ -0,0 +1,30 @@
+package com.github.nianliu.creational_patterns
+
+interface Game
+
+data class SwitchGame(val name: String) : Game
+
+data class PS4Game(val name: String) : Game
+
+class GameFactory {
+
+    fun makeGame(name: String, platform: String): Game {
+        return when (platform.toLowerCase()) {
+            "switch" -> SwitchGame(name)
+            "ps4" -> PS4Game(name)
+            else -> throw IllegalArgumentException("Unknown platform: $platform")
+        }
+    }
+
+}
+
+fun main() {
+    val gameFactory = GameFactory()
+    val games = arrayOf(
+        gameFactory.makeGame("Pokeman", "switch")
+        , gameFactory.makeGame("Assassin's Creed", "ps4")
+        , gameFactory.makeGame("FIFA 2020", "ps4")
+    )
+
+    games.forEach { println(it) }
+}
